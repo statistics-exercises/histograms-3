68f2298e547d5073455eb97d0ceb420d27ea0c62
Gareth Tribello <gareth.tribello@gmail.com>
2020-10-30 10:03:06 +0000

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def myvariable( probs ) :
   if myvar < probs[0] + probs[1] : return 1
   # You will need to write the rest of this function here
   
-probs = [0.5, 0.1, ]  # You need to fill in the rest of this vector
+probs = np.array([0.5, 0.1, ])  # You need to fill in the rest of this vector
 expectation, variance = 0, 0
 # You need to add code to calculate the expectation and the variance from
 # the vector of probabilities given above here.
