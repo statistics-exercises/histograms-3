1de341d2478cb54dbed95cc8df50abbb8f5dc1c2
Gareth Tribello <gareth.tribello@gmail.com>
2021-07-15 12:36:21 +0100

    Added answer

--- main.py
+++ main.py
@@ -4,7 +4,9 @@ def myvariable( probs ) :
   myvar = np.random.uniform(0,1) 
   if myvar < probs[0] : return 0
   if myvar < probs[0] + probs[1] : return 1
-  # You will need to write the rest of this function here
+  if myvar < probs[0] + probs[1] + probs[2] : return 2 
+  if myvar < probs[0] + probs[1] + probs[2] + probs[3] : return 3
+  return 4 
   
-probs = np.array([0.5, 0.1, ])  # You need to fill in the rest of this vector
+probs = np.array([0.5, 0.1, 0.2, 0.05, 0.15 ])  # You need to fill in the rest of this vector
 print( myvariable( probs ), myvariable( probs ), myvariable( probs ) )
